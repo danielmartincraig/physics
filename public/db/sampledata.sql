ce853f23661bc6a1381eaf62bedc9a49a9404a06
danielmartincraig <danielmartincraig@gmail.com>
2018-07-14 01:57:01 -0600

    Added the query to the quotes page

---
+++ public/db/sampledata.sql
@@ -0,0 +1,8 @@
+INSERT INTO quotes
+( quote_text
+, quote_author
+)
+VALUES
+( 'I was born not knowing and have had only a little time to change that here and there.'
+, 'Richard P. Feynman'
+);
--- server.js
+++ server.js
@@ -2,11 +2,10 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT;
 const path = require('path');
-const { Client } = require('pg');
+const { Pool } = require('pg');
 
 const connectionString = process.env.DATABASE_URL;
-const client = new Client({connectionString});
-client.connect();
+const pool = new Pool({connectionString: connectionString});
 
 app.use(express.static(path.join(__dirname, 'public')))
     .set('views', path.join(__dirname, 'views'))
@@ -15,6 +14,20 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.get('/', (req, res) => res.render('physics/index'))
     .get('/sim', (req, res) => res.render('physics/sim'))
     .get('/highscores', (req, res) => res.render('physics/highscores'))
-    .get('/quotes', (req, res) => res.render('physics/quotes'));
+    .get('/quotes', handleQuotesRequest);
 
 app.listen(PORT);
+
+function handleQuotesRequest(req, res) {
+    let quotesQuery = "SELECT quote_text, quote_author FROM quotes";
+    pool.query(quotesQuery, function (err, result) {
+        if (err) {
+            throw err;
+        }
+
+        app.locals.quotes = result.rows;
+        res.render('physics/quotes');
+    });
+
+
+}
