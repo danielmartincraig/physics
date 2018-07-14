20b6e706aad2e90d4ee093cf6530cc1c57cdd157
danielmartincraig <danielmartincraig@gmail.com>
2018-07-14 02:52:28 -0600

    added handler for high scores get requests

--- public/db/sampledata.sql
+++ public/db/sampledata.sql
@@ -6,3 +6,23 @@ VALUES
 ( 'I was born not knowing and have had only a little time to change that here and there.'
 , 'Richard P. Feynman'
 );
+
+INSERT INTO quotes
+( quote_text
+, quote_author
+)
+VALUES
+( 'All of physics is either impossible or trivial. It is impossible until you understand it, and then it becomes trivial.'
+, 'Ernest Rutherford'
+);
+
+INSERT INTO quotes
+( quote_text
+, quote_author
+)
+VALUES
+( 'In its efforts to learn as much as possible about nature, modern physics has found that certain things can never be “known” with certainty. Much of our knowledge must always remain uncertain. The most we can know is in terms of probabilities.'
+, 'Richard P. Feynman'
+);
+
+
--- server.js
+++ server.js
@@ -13,10 +13,22 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => res.render('physics/index'))
     .get('/sim', (req, res) => res.render('physics/sim'))
-    .get('/highscores', (req, res) => res.render('physics/highscores'))
+    .get('/highscores', handleHighScoresRequest)
     .get('/quotes', handleQuotesRequest);
 
 app.listen(PORT);
+
+function handleHighScoresRequest(req, res) {
+    let highScoresQuery = "SELECT final_distance, player_initials FROM highscores h INNER JOIN trajectories t " +
+        "ON h.trajectory_id = t.id";
+    pool.query(highScoresQuery, function (err, result) {
+        if (err) {
+            throw err;
+        }
+        app.locals.highscores = result.rows;
+        res.render('physics/highscores');
+    });
+}
 
 function handleQuotesRequest(req, res) {
     let quotesQuery = "SELECT quote_text, quote_author FROM quotes";
